1ef6a271580936b3a7bf8e01b814abd0c7d0259b
Alan Mathew <alan.mathew.76@gmail.com>
2021-07-22 15:34:49 -0400

    added anti-blocking

--- scripts/01_webCrawler.py
+++ scripts/01_webCrawler.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import os
 import time
 import random
 import pickle
@@ -17,23 +18,52 @@ reqs = requests.get('https://proxy.webshare.io/api/proxy/list/', headers=key)
 
 proxy_list = reqs.json()['results']
 proxy_list = list(map(lambda x: f"http://{x['username']}:{x['password']}@{x['proxy_address']}:{x['ports']['http']}/", proxy_list))
+random.shuffle(proxy_list)
 proxy_cycle = cycle(proxy_list)
+
+user_agent_list = [
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+]
 
 # urls = {'novel':{'https://www.anime-planet.com/'}, 'done':set(), 'batch':[]}
 with open('../data/urls.pkl','rb') as file:
     urls = pickle.load(file)  
-    
+
 def saveUrls():
     with open('../data/urls.pkl','wb') as file:
         pickle.dump(urls, file)
 
 def getCurrentPageUrls(url):
-    time.sleep(random.randint(200,1000)/1000)
+    global proxy_cycle
+    time.sleep(random.randint(1000,3000)/1000)
     cur_urls = set()
     
     proxy = next(proxy_cycle)
-    reqs = requests.get(url, proxies={'http': proxy, 'https': proxy})
-    soup = BeautifulSoup(reqs.text, 'html.parser')
+    user_agent = random.choice(user_agent_list)
+    headers = {'User-Agent': user_agent}
+    try:
+        reqs = requests.get(url, proxies={'http': proxy, 'https': proxy}, headers=headers)
+    except:
+        print(proxy)
+        proxy_list.remove(proxy)
+        proxy_cycle = cycle(proxy_list)
+        return getCurrentPageUrls(url)
+    
+    if reqs.status_code != 200:
+#         urls['novel'] = urls['novel'].union(urls['batch'])
+#         urls['novel'].add(url)
+        print(proxy)
+        proxy_list.remove(proxy)
+        proxy_cycle = cycle(proxy_list)
+        return getCurrentPageUrls(url)
+        
+    
+    html_text = reqs.text
+    soup = BeautifulSoup(html_text, 'html.parser')
 
     for link in soup.find_all('a'):
         try:
@@ -43,33 +73,43 @@ def getCurrentPageUrls(url):
         except:
             pass
     
-    if len(cur_urls) == 0:
-        urls['novel'] = urls['novel'].union(urls['batch'])
-        raise Exception('Scrape Unsucessful')
-    
-    return cur_urls
+    return cur_urls, {url: html_text}
 
 def getAllUrls():
     counter = 0
+    page_data = {'url':[], 'html_text':[]}
     while len(urls['novel']) > 0:
-#        if len(urls['novel']) >= 16:
-#            for _ in range(16):
-#                pop_url = urls['novel'].pop()
-#                if pop_url not in urls['done']:
-#                    urls['batch'].append(pop_url)
-#
-#            with Pool(16) as p:
-#                cur_urls = p.map(getCurrentPageUrls, urls['batch'])
-#
-#            cur_urls = set().union(*cur_urls)
-#
-#            urls['done'] = urls['done'].union(urls['batch'])
-#            
-#        else:
+        
+        # if len(urls['novel']) >= 16:
+        #     while len(urls['batch']) < 16:
+        #         pop_url = urls['novel'].pop()
+        #         if pop_url not in urls['done']:
+        #             urls['batch'].append(pop_url)
+
+        #     with Pool(16) as p:
+        #         output = p.map(getCurrentPageUrls, urls['batch'])
+
+        #     cur_urls, url_html_text_list = [item[0] for item in output], [item[1] for item in output]
+        #     cur_urls = set().union(*cur_urls)
+
+        #     url_html_text = {}
+        #     for d in url_html_text_list:
+        #         url_html_text.update(d)
+
+        #     for key in url_html_text.keys():
+        #         urls['done'].add(key)
+        #         page_data['url'].append(key)
+        #         page_data['html_text'].append(url_html_text[key])
+            
+        #     urls['batch'] = []
+
+        # else:
         pop_url = urls['novel'].pop()
         if pop_url not in urls['done']:
-            cur_urls = getCurrentPageUrls(pop_url)
+            cur_urls, url_html_text = getCurrentPageUrls(pop_url)
             urls['done'].add(pop_url)
+            page_data['url'].append(pop_url)
+            page_data['html_text'].append(url_html_text[pop_url])
 
         
         print(len(urls['novel']), len(urls['done']), 0 if len(urls['novel']) == 0 else len(urls['done'])/len(urls['novel']))
@@ -79,8 +119,13 @@ def getAllUrls():
         
         counter += 1; 
 
-        if counter == 10:
+        if counter % 10 == 0:
             saveUrls()
-            counter = 0
+            page_df = pd.DataFrame(page_data)
+            page_df.to_csv('../data/page_data.csv', mode='a', index=False,
+                           header=not os.path.exists('../data/page_data.csv'))
+                
+            del page_df
+            page_data = {'url':[], 'html_text':[]}
 
 getAllUrls()
