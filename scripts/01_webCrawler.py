e59f11cd8eeafdc0398d94ab3313593d9a7e0e68
Alan Mathew <alan.mathew.76@gmail.com>
2021-08-02 13:20:19 -0400

    experiencing unqiueness issues

--- scripts/01_webCrawler.py
+++ scripts/01_webCrawler.py
@@ -1,5 +1,3 @@
-hard_drive = 'sdb'
-
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
@@ -30,31 +28,52 @@ db_string = f"postgresql://{username}:{password}@192.168.0.3:5432/animeplanet"
 db = create_engine(db_string)
 
 def loadData():
+    print('loading data...')
     urls = {}
     with db.connect() as con:
-        urls['done'] = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NOT NULL', con)['url'].to_list())
-        urls['to_do'] = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NULL', con)['url'].to_list())
+        urls['done'] = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NOT NULL;', con)['url'].to_list())
+        urls['todo'] = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NULL;', con)['url'].to_list())
+        
     return urls
 
-urls = loadData()
-
-def saveData(page_data):
+def saveData(page_data, urls):
+    print('saving data...')
+    
     with open('../data/urls.pkl','wb') as file:
         pickle.dump(urls, file)
         
     with open("../data/urls.json", 'w') as file:
-        json.dump({'done':list(urls['done']), 'to_do':list(urls['to_do'])}, file, indent=2) 
+        json.dump({'done':list(urls['done']), 'todo':list(urls['todo'])}, file, indent=2) 
         
     page_df = pd.DataFrame(page_data)
+    done_df = page_df.loc[page_df['html_text'].notnull()]
+    todo_df = page_df.loc[page_df['html_text'].isnull()]
     
     with db.connect() as con:
-        page_df.to_sql('web_scrape', con, index=False, if_exists='append', method='multi')
+        print('\tremoving popped todo data...')
+        con.execute(f"DELETE FROM web_scrape WHERE url in ({str(done_df['url'].to_list())[1:-1]})")
+        
+        print('\tsaving done data...')
+        done_df.to_sql('web_scrape', con, index=False, if_exists='append')
+#         for i in tqdm(range(done_df.shape[0])):
+#             try:
+#                 done_df.iloc[i:i+1].to_sql('web_scrape', con, index=False, if_exists='append')
+#             except:
+#                 pass
+
+        print('\tsaving todo data...')
+        todo_df.to_sql('web_scrape', con, index=False, if_exists='append')
+#         for i in tqdm(range(todo_df.shape[0])):
+#             try:
+#                 todo_df.iloc[i:i+1].to_sql('web_scrape_todo', con, index=False, if_exists='append')
+#             except:
+#                 pass
         
     del page_df
 
 sr = specialRequests()
 
-def getCurrentPageUrls(url):    
+def getPage(url):    
     
     time.sleep(random.randint(200, 2000)/1000)
     
@@ -99,58 +118,61 @@ def getCurrentPageUrls(url):
 #                 saveData(page_data)
 #                 page_data = {'url':[], 'html_text':[]}
 
-def getAllUrlsMulti(urls):
+def getAllPagesMulti():
+    urls = loadData()
+    print('starting crawl...')
     page_data = {'url':[], 'html_text':[]}
     disallowed_urls = ['https://www.anime-planet.com/search.php', 'https://www.anime-planet.com/login',
                        'https://www.anime-planet.com/sign-up']
     start_time = time.time()
-    first = True
-    while len(urls['to_do']) > 0:
+    while len(urls['todo']) > 0:
+        dist_to25 = (25 - (len(urls['done']) % 25))
+        
         popped_urls = []
-        
-        dist_to25 = 25 - (len(urls['done']) % 25)
-        
         while len(popped_urls) < dist_to25:
-            pop_url = urls['to_do'].pop()
-
+            pop_url = urls['todo'].pop()
+            
             if pop_url[-1] == '.':
+                old_url = pop_url
                 pop_url = pop_url.replace('forum/members', 'users')[:-1]
+                urls['done'].add(old_url)
+                page_data['url'].append(old_url)
+                page_data['html_text'].append('failed scrape')
 
             if (pop_url not in urls['done']) and (pop_url not in disallowed_urls):
                 popped_urls.append(pop_url)
 
         with ThreadPoolExecutor(max_workers=25) as executor:
-            results = list(executor.map(getCurrentPageUrls, popped_urls))
+            results = list(executor.map(getPage, popped_urls))
 
-
-        urls['done'] = urls['done'].union(popped_urls)
+        urls['done'] = urls['done'].union(set(popped_urls))
 
         cur_urls = set().union(*[item[0] for item in results])
         list_of_tuples = [item[1] for item in results]
-        url_html_dict = {}
         for tup in list_of_tuples:
             page_data['url'].append(tup[0])
-            page_data['html_text'].append(tup[1])
+            if tup[1] == '':
+                page_data['html_text'].append('failed scrape')
+            else:
+                page_data['html_text'].append(tup[1])
 
-        novel = cur_urls.difference(urls['done'])
-        urls['to_do'] = urls['to_do'].union(novel)
+        novel = (cur_urls.difference(urls['done'])).difference(urls['todo'])
+        urls['todo'] = urls['todo'].union(novel)
         
         for link in novel:
             page_data['url'].append(link)
             page_data['html_text'].append(np.NaN)
         
-        print(len(urls['to_do']), len(urls['done']), 0 if len(urls['to_do']) == 0 else len(urls['done'])/len(urls['to_do']))
+        print(len(urls['todo']), len(urls['done']), 0 if len(urls['todo']) == 0 else len(urls['done'])/(len(urls['todo'])+len(urls['done'])))
 
         len_done = len(urls['done'])
         if len_done % 500 == 0:
             end_time = time.time()
             print('timer: ', end_time-start_time)
-            print('saving data...')
-            saveData(page_data)
-            if len_done % 10000 == 0:
-                urls = loadData()
+            saveData(page_data, urls)
+            urls = loadData()
             page_data = {'url':[], 'html_text':[]}
-            time.sleep(random.randint(3000, 6000)/1000)
+            print('starting crawl...')
             start_time = time.time()
 
-getAllUrlsMulti(urls)
+getAllPagesMulti()
