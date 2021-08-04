5552274e36ec3896f23527326af78a5a044d37ed
Alan Mathew <alan.mathew.76@gmail.com>
2021-08-03 23:19:57 -0400

    solved uniqueness issue

--- scripts/01_webCrawler.py
+++ scripts/01_webCrawler.py
@@ -1,85 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import requests
 from bs4 import BeautifulSoup
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-import os
 import sys
 import time
 import random
 import pickle
 import json
 from itertools import cycle
-from multiprocessing import Pool
 from concurrent.futures import ThreadPoolExecutor
-import sqlalchemy as db
 from sqlalchemy import create_engine
 
 sys.path.insert(0, '../tools/')
 from specialRequests import specialRequests
 
-with open('../tools/credentials.json') as file:
-    credentials = json.load(file)
-    
-username = credentials["dblogin"]["username"]
-password = credentials["dblogin"]["password"]
 
-db_string = f"postgresql://{username}:{password}@192.168.0.3:5432/animeplanet"
-db = create_engine(db_string)
+# In[2]:
 
-def loadData():
-    print('loading data...')
-    urls = {}
-    with db.connect() as con:
-        urls['done'] = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NOT NULL;', con)['url'].to_list())
-        urls['todo'] = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NULL;', con)['url'].to_list())
+
+class AnimePlanetCrawler:
+    def __init__(self):
+        with open('../tools/credentials.json') as file:
+            credentials = json.load(file)
+
+        username = credentials["dblogin"]["username"]
+        password = credentials["dblogin"]["password"]
+
+        db_string = f"postgresql://{username}:{password}@192.168.0.3:5432/animeplanet"
+        self.db = create_engine(db_string)
         
-    return urls
+        self.sr = specialRequests()
 
-def saveData(page_data, urls):
+
+# In[3]:
+
+
+def loadData(self):
+    print('loading data...')
+    with self.db.connect() as con:
+        self.done = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NOT NULL;', con)['url'].to_list())
+        self.pending = set(pd.read_sql('SELECT url FROM web_scrape WHERE html_text IS NULL;', con)['url'].to_list())
+        self.novel = set()
+        self.batch = {}
+
+
+# In[4]:
+
+
+def saveData(self):
     print('saving data...')
     
-    with open('../data/urls.pkl','wb') as file:
-        pickle.dump(urls, file)
-        
-    with open("../data/urls.json", 'w') as file:
-        json.dump({'done':list(urls['done']), 'todo':list(urls['todo'])}, file, indent=2) 
-        
-    page_df = pd.DataFrame(page_data)
-    done_df = page_df.loc[page_df['html_text'].notnull()]
-    todo_df = page_df.loc[page_df['html_text'].isnull()]
+    self.novel = self.novel.difference(set(self.batch.keys()))
+    self.pending = self.pending.difference(set(self.batch.keys()))
     
-    with db.connect() as con:
-        print('\tremoving popped todo data...')
-        con.execute(f"DELETE FROM web_scrape WHERE url in ({str(done_df['url'].to_list())[1:-1]})")
+    data_dict = {'done': list(self.done), 
+                 'pending': list(self.pending),
+                 'novel': list(self.novel),
+                 'batch': self.batch}
+    
+    with open('../data/urls.pkl','wb') as file:
+        pickle.dump(data_dict, file)
+    
+    with open("../data/urls.json", 'w') as file:
+        json.dump(data_dict, file, indent=2) 
+        
+    batch_dict = {'url': list(self.batch.keys()),
+                  'html_text': list(self.batch.values())}
+    batch_df = pd.DataFrame(batch_dict)
+    
+    novel_dict = {'url': list(self.novel),
+                  'html_text': [np.NaN for _ in range(len(self.novel))]}
+    novel_df = pd.DataFrame(novel_dict)
+    
+    batch_urls = batch_dict['url']
+    novel_urls = novel_dict['url']
+    
+    inter = set(batch_urls).intersection(set(novel_urls))
+    
+    if (len(inter) != 0):
+        print('inter:', inter)
+    
+    with self.db.connect() as con:
+        print('\tremoving popped pending data...')
+        con.execute(f"DELETE FROM web_scrape WHERE url in ({str(batch_urls)[1:-1]})")
         
         print('\tsaving done data...')
-        done_df.to_sql('web_scrape', con, index=False, if_exists='append')
-#         for i in tqdm(range(done_df.shape[0])):
-#             try:
-#                 done_df.iloc[i:i+1].to_sql('web_scrape', con, index=False, if_exists='append')
-#             except:
-#                 pass
+        batch_df.to_sql('web_scrape', con, index=False, if_exists='append')
 
-        print('\tsaving todo data...')
-        todo_df.to_sql('web_scrape', con, index=False, if_exists='append')
-#         for i in tqdm(range(todo_df.shape[0])):
-#             try:
-#                 todo_df.iloc[i:i+1].to_sql('web_scrape_todo', con, index=False, if_exists='append')
-#             except:
-#                 pass
+        try:
+            print('\tsaving pending data...')
+            novel_df.to_sql('web_scrape', con, index=False, if_exists='append')
+        except Exception as e: 
+            print(e)
+            con.execute(f"UPDATE web_scrape SET html_text = NULL WHERE url IN ({str(batch_urls)[1:-1]})")
+            self.done = self.done.difference(batch_urls)
+            self.pending = self.pending.difference(novel_urls)
+            self.pending = self.pending.union(batch_urls)
         
-    del page_df
+    self.batch = {}
+    self.novel = set()
 
-sr = specialRequests()
 
-def getPage(url):    
-    
-    time.sleep(random.randint(200, 2000)/1000)
-    
+# In[5]:
+
+
+def scrapePage(self, url):    
+ 
     cur_urls = set()
 
-    html_text = sr.get(url)
+    html_text = self.sr.get(url)
     soup = BeautifulSoup(html_text, 'html.parser')
 
     for link in soup.find_all('a'):
@@ -92,87 +128,110 @@ def getPage(url):
     
     return cur_urls, (url, html_text)
 
-# def getAllUrls():
-#     page_data = {'url':[], 'html_text':[]}
-#     disallowed_urls = ['https://www.anime-planet.com/search.php', 'https://www.anime-planet.com/login',
-#                        'https://www.anime-planet.com/sign-up']
-#     while len(urls['novel']) > 0:
-#         pop_url = urls['novel'].pop()
 
-#         if pop_url[-1] == '.':
-#             pop_url = pop_url.replace('forum/members', 'users')[:-1]
+# In[6]:
 
-#         if (pop_url not in urls['done']) and (pop_url not in disallowed_urls):
-#             cur_urls, html_text = getCurrentPageUrls(pop_url)
-#             urls['done'].add(pop_url)
-#             page_data['url'].append(pop_url)
-#             page_data['html_text'].append(html_text)
-        
-#             diff = cur_urls.difference(urls['done'])
-#             urls['novel'] = urls['novel'].union(diff)
-        
-#             print(len(urls['novel']), len(urls['done']), 0 if len(urls['novel']) == 0 else len(urls['done'])/len(urls['novel']), pop_url)
-        
-#             if len(urls['done']) % 100 == 0:
-#                 print('saving data...')
-#                 saveData(page_data)
-#                 page_data = {'url':[], 'html_text':[]}
 
-def getAllPagesMulti():
-    urls = loadData()
-    print('starting crawl...')
-    page_data = {'url':[], 'html_text':[]}
-    disallowed_urls = ['https://www.anime-planet.com/search.php', 'https://www.anime-planet.com/login',
+def popBatch(self):
+    disallowed_urls = ['https://www.anime-planet.com/search.php', 'https://www.anime-planet.com/login', 
                        'https://www.anime-planet.com/sign-up']
-    start_time = time.time()
-    while len(urls['todo']) > 0:
-        dist_to25 = (25 - (len(urls['done']) % 25))
-        
-        popped_urls = []
-        while len(popped_urls) < dist_to25:
-            pop_url = urls['todo'].pop()
+    
+    dist_to25 = (25 - (len(self.done) % 25))
+    
+    popped_urls = set()
+    while len(popped_urls) < dist_to25:
+        pop_url = self.pending.pop()
+
+        if pop_url[-1] == '.':
+            new_url = pop_url
+            new_url = new_url.replace('forum/members', 'users')[:-1]
+            if (new_url not in self.done) and (new_url not in disallowed_urls):
+                popped_urls.add(new_url)
+                
+        if (pop_url not in self.done) and (pop_url not in disallowed_urls):
+            popped_urls.add(pop_url)
             
-            if pop_url[-1] == '.':
-                old_url = pop_url
-                pop_url = pop_url.replace('forum/members', 'users')[:-1]
-                urls['done'].add(old_url)
-                page_data['url'].append(old_url)
-                page_data['html_text'].append('failed scrape')
+    return popped_urls
 
-            if (pop_url not in urls['done']) and (pop_url not in disallowed_urls):
-                popped_urls.append(pop_url)
 
+# In[7]:
+
+
+def processCrawlResults(self, results):
+    cur_urls = set().union(*map(lambda x: x[0], results))
+    url_html_tup = map(lambda x: x[1], results)
+    for url, html_text in url_html_tup:
+        self.done.add(url)
+        self.batch[url] = 'failed scrape' if html_text == '' else html_text
+    
+    cur_urls = (cur_urls.difference(self.pending)).difference(self.done)
+    self.novel = self.novel.union(cur_urls)
+    self.pending = self.pending.union(cur_urls)
+
+
+# In[8]:
+
+
+def printCrawlProgress(self):
+    len_done = len(self.done)
+    len_pending = len(self.pending)
+    print(len_pending, len_done, 0 if len_pending == 0 else len_done/(len_pending+len_done))
+    return len_done
+
+
+# In[9]:
+
+
+def crawl(self):
+    self.loadData()
+    print('starting crawl...')
+    start_time = time.time()
+    while len(self.pending) > 0:
+
+        
+        popped_urls = self.popBatch()    
+            
         with ThreadPoolExecutor(max_workers=25) as executor:
-            results = list(executor.map(getPage, popped_urls))
-
-        urls['done'] = urls['done'].union(set(popped_urls))
-
-        cur_urls = set().union(*[item[0] for item in results])
-        list_of_tuples = [item[1] for item in results]
-        for tup in list_of_tuples:
-            page_data['url'].append(tup[0])
-            if tup[1] == '':
-                page_data['html_text'].append('failed scrape')
-            else:
-                page_data['html_text'].append(tup[1])
-
-        novel = (cur_urls.difference(urls['done'])).difference(urls['todo'])
-        urls['todo'] = urls['todo'].union(novel)
+            results = list(executor.map(self.scrapePage, popped_urls))
         
-        for link in novel:
-            page_data['url'].append(link)
-            page_data['html_text'].append(np.NaN)
+        self.processCrawlResults(results)
         
-        print(len(urls['todo']), len(urls['done']), 0 if len(urls['todo']) == 0 else len(urls['done'])/(len(urls['todo'])+len(urls['done'])))
-
-        len_done = len(urls['done'])
+        len_done = self.printCrawlProgress()
+        
         if len_done % 500 == 0:
             end_time = time.time()
             print('timer: ', end_time-start_time)
-            saveData(page_data, urls)
-            urls = loadData()
-            page_data = {'url':[], 'html_text':[]}
+            self.saveData()
             print('starting crawl...')
             start_time = time.time()
 
-getAllPagesMulti()
+
+# In[10]:
+
+
+AnimePlanetCrawler.loadData = loadData
+AnimePlanetCrawler.saveData = saveData
+AnimePlanetCrawler.scrapePage = scrapePage
+AnimePlanetCrawler.popBatch = popBatch
+AnimePlanetCrawler.processCrawlResults = processCrawlResults
+AnimePlanetCrawler.printCrawlProgress = printCrawlProgress
+AnimePlanetCrawler.crawl = crawl
+
+
+# In[11]:
+
+
+crawler = AnimePlanetCrawler()
+
+
+# In[12]:
+
+
+crawler.crawl()
+
+
+# In[ ]:
+
+
+
+
